2c0664730ad0fab55f2a4e6e4fead52c5a7b49c9
Bread <jmagri1@uncc.edu>
2024-10-17 21:54:30 -0400

    Build historyService for performing CRUD on json database file

--- Develop/server/src/routes/htmlRoutes.ts
+++ Develop/server/src/routes/htmlRoutes.ts
@@ -1,10 +1,11 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import express, { type Request, type Response } from 'express';
 import { Router } from 'express';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = Router();
 
 // TODO: Define route to serve index.html
-
+router.use(express.static('../../client/dist/index.html'));
 export default router;
