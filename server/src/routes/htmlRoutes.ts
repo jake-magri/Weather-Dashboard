e39907df4b04e3987eddfb80d1ec0d2466006da7
Bread <jmagri1@uncc.edu>
2024-10-20 20:10:27 -0400

    Render deployment debugging

--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -8,8 +8,6 @@ const __dirname = path.dirname(__filename); // gets directory name of current mo
 const router = Router(); // inilitalize router with routes
 
 // Define route to serve index.html
-router.use(express.static(path.join(__dirname, '../../../client/dist/index.html')));
-
 //route to serve index.html for the root path
 router.get('/', (_req: Request, res: Response) => {
     console.log('GET request received at root path.');
