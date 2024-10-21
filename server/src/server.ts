732a5c88ec1b1f2aa861271935f2641f567d49e3
Bread <jmagri1@uncc.edu>
2024-10-20 20:58:57 -0400

    Render Debug: Corrected static files expression

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,12 @@
 import dotenv from 'dotenv'; // dotenv for reading port number from environment file
 import express, {} from 'express';
+import path from 'path';
 dotenv.config(); // calling config reads .env file and loads variables int process.env
+
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url); // gives the url of the current module
+const __dirname = path.dirname(__filename); // gets directory name of current module file
 
 // Import the routes
 import routes from './routes/index.js';
@@ -10,7 +16,7 @@ const app = express(); // initialize express
 const PORT = process.env.PORT || 3001; // assigns the PORT for use
 console.log('GET request received at server.ts.');
 // Serve static files of entire client dist folder
-app.use(express.static('../../client/dist'));
+app.use(express.static((path.join(__dirname, '../../client/dist'))));
 // middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
